c3f223343bd292ed5f8eec37696e19acef8883e5
vladikNovla <zyymer20001000@gmail.com>
2024-10-20 20:52:41 +0300

    Remove done

--- ConsoleAppList/MyList.cs
+++ ConsoleAppList/MyList.cs
@@ -1,4 +1,7 @@
 using System.Dynamic;
+using System.Collections.ObjectModel;
+using System.Runtime.ConstrainedExecution;
+using System.Diagnostics;
 
 public class MyList()
 {
@@ -26,15 +29,67 @@ public class MyList()
     }
     public void Remove(int rem)
     {
-
+        if (_counter == 0)
+        {
+            throw new ArgumentOutOfRangeException($"{_counter} - _counter, список не содержет элементов");
+        }
+        // обработка случая, когда список пуст
+        int ind = -1;
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            if (arr[i] == rem)
+            {
+                ind = i;
+                break;
+            }
+        }
+        // метод находит индект элемента и удаляет его методом RemoveAt
+        RemoveAt(ind);
     }
-    public void RemoveAt(int rem, int index)
+    public void RemoveAt(int index)
     {
-
+        if (_counter == 0)
+        {
+            throw new ArgumentOutOfRangeException($"{index} - index, список не содержет элементов");
+        }
+        // обработка случая, когда список пуст
+        _counter--;
+        if (_counter == 0)
+        {
+            Clear();
+        }
+        else
+        {
+            int[] temp1 = new int[index];
+            int[] temp2 = new int[arr.Length - (index + 1)];
+            for (int i = 0; i < index; i++)
+            {
+                temp1[i] = arr[i];
+            }
+            for (int i = index + 1; i < arr.Length; i++)
+            {
+                temp2[i - (index + 1)] = arr[i];
+            }
+            // элементы основного массива делятся на 2 временных за исключением искомого
+            int[] temp3 = new int[temp1.Length + temp2.Length];
+            for (int i = 0; i < temp1.Length; i++)
+            {
+                temp3[i] = temp1[i];
+            }
+            for (int i = temp1.Length; i < temp3.Length; i++)
+            {
+                temp3[i] = temp2[i - temp1.Length];
+            }
+            // временные массивы склеиваются в один и клонируются в основной
+            arr = (int[])temp3.Clone();
+        }
     }
     public void Clear()
     {
-
+        int[] temp = new int[0];
+        arr = (int[])temp.Clone();
+        _counter = 0;
+        // присваивание массиву новый пустой массив
     }
     public string ListToString()
     {

c3265e298b808abed83bea5c21c3014a0ada21d6
vladikNovla <zyymer20001000@gmail.com>
2024-10-24 18:00:57 +0300

    toString and memory fix

--- ConsoleAppList/MyList.cs
+++ ConsoleAppList/MyList.cs
@@ -2,60 +2,46 @@ using System.Dynamic;
 using System.Collections.ObjectModel;
 using System.Runtime.ConstrainedExecution;
 using System.Diagnostics;
+using System.Data.Common;
 
 public class MyList()
 {
-    int[] arr = new int[0];
+    int[] arr = new int[4];
     int _counter;
-    public int Count { get { return _counter; } set { _counter = value; } }
+    public int Count { get { return _counter; }}
 
     public void Add(int add)
     {
         _counter++;
-        int[] temp = new int[_counter];
-        // создание нового массива с размером на один элемент больше
-        for (int i = 0; i < arr.Length; i++)
+        if (_counter > arr.Length)
         {
-            temp[i] = arr[i];
-            // перенос элементов во временный массив
+            Resize();
         }
-        temp[_counter - 1] = add;
-        arr = temp;
-        // добавление нового элемента и клонирование временного масива
+        arr[_counter - 1] = add;
     }
     public void Insert(int add, int index)
     {
         _counter++;
-        int[] temp1 = new int[index + 1];
-        for (int i = 0; i < index; i++)
+        if (_counter > arr.Length)
         {
-            temp1[i] = arr[i];
+            Resize();
         }
-        temp1[index] = add;
-        // вкладываем в первый временный массив часть исходного и дополнительный элемент
-        int[] temp2 = new int[arr.Length - (temp1.Length - 1)];
-        for (int i = index; i < arr.Length; i++)
+        int[] temp1 = new int[_counter - index];
+        for (int i = index; i < _counter; i++)
         {
-            temp2[i - index] = arr[i];
+            temp1[i - index] = arr[i];
         }
-        // переписываем отстатки записываем во второй временный массив
-        int[] temp3 = new int[temp1.Length + temp2.Length];
-        for (int i = 0; i < temp1.Length; i++)
+        arr[index] = add;
+        for (int i = index + 1; i < _counter; i++)
         {
-            temp3[i] = temp1[i];
+            arr[i] = temp1[i - (index + 1)];
         }
-        for (int i = temp1.Length; i < temp3.Length; i++)
-        {
-            temp3[i] = temp2[i - temp1.Length];
-        }
-        // временные массивы склеиваются в один и клонируются в основной
-        arr = temp3;
     }
     public void Remove(int rem)
     {
         if (_counter == 0)
         {
-            throw new ArgumentOutOfRangeException($"{_counter} - _counter, список не содержет элементов");
+            throw new ArgumentOutOfRangeException("Список не содержет элементов");
         }
         // обработка случая, когда список пуст
         int ind = -1;
@@ -74,7 +60,7 @@ public class MyList()
     {
         if (_counter == 0)
         {
-            throw new ArgumentOutOfRangeException($"{index} - index, список не содержет элементов");
+            throw new ArgumentOutOfRangeException("Список не содержет элементов");
         }
         // обработка случая, когда список пуст
         _counter--;
@@ -84,40 +70,45 @@ public class MyList()
         }
         else
         {
-            int[] temp1 = new int[index];
-            int[] temp2 = new int[arr.Length - (index + 1)];
-            for (int i = 0; i < index; i++)
+            int[] temp1 = new int[_counter - index];
+            for (int i = index + 1; i < _counter + 1; i++)
             {
-                temp1[i] = arr[i];
+                temp1[i - (index + 1)] = arr[i];
             }
-            for (int i = index + 1; i < arr.Length; i++)
+            for (int i = index; i < _counter; i++)
             {
-                temp2[i - (index + 1)] = arr[i];
+                arr[i] = temp1[i - index];
             }
-            // элементы основного массива делятся на 2 временных за исключением искомого
-            int[] temp3 = new int[temp1.Length + temp2.Length];
-            for (int i = 0; i < temp1.Length; i++)
-            {
-                temp3[i] = temp1[i];
-            }
-            for (int i = temp1.Length; i < temp3.Length; i++)
-            {
-                temp3[i] = temp2[i - temp1.Length];
-            }
-            // временные массивы склеиваются в один и клонируются в основной
-            arr = temp3;
         }
     }
     public void Clear()
     {
-        int[] temp = new int[0];
-        arr = temp;
+        arr = new int[4];
         _counter = 0;
-        // присваивание массиву новый пустой массив
+        // присваиваем массиву новый пустой массив
+    }
+    public void Resize()
+    {
+        int[] arrTemp = new int[arr.Length * 2];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arrTemp[i] = arr[i];
+        }
+        arr = arrTemp;
+        // выделение нового места для элементов списка
     }
     public string ListToString()
     {
-        string s = string.Join(", ", arr);
+        string s = "";
+        for (int i = 0; i < _counter; i++)
+        {
+            s += arr[i];
+            if (i + 1 != _counter)
+            {
+                s += ", ";
+            }
+        }
+        // вывод элементов списка в виде строки
         return s;
     }
 }
